ab926afbe646c550d2568f6115059ffdf9cd97f5
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2022-09-08 21:46:47 +0400

    messages created

---
+++ src/demoProjects/Kodlama.io.Devs/Application/Features/Technologies/Constants/Messages/TechnologyMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Technologies.Constants.Messages
+{
+    public static  class TechnologyMessages
+    {
+        public static string NameExists = "Technology name exists .";
+        public static string RequestedDoesNotExists = "Requested Technology does not exist .";
+    }
+}
