b9d86eb12f32775fbccd486dd3a1c4c7e45132bf
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2022-09-11 16:52:25 +0400

    updated

---
+++ src/demoProjects/Kodlama.io.Devs/Application/Features/UserSocialMediaAddresses/Commands/Update/UpdateUserSocialMediaAddressCommand.cs
@@ -0,0 +1,60 @@
+﻿using Application.Features.Authorizations.Rules;
+using Application.Features.UserSocialMediaAddresses.Dtos;
+using Application.Features.UserSocialMediaAddresses.Models.Commands;
+using Application.Features.UserSocialMediaAddresses.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Pipelines.Authorization;
+using Core.Security.Entities;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.UserSocialMediaAddresses.Commands.Update
+{
+    public class UpdateUserSocialMediaAddressCommand : IRequest<UpdatedUserSocialMediaAddressDto>,
+                                                      ISecuredRequest
+    {
+        public int Id { get; set; }
+        public UpdateUserSocialMediaAddressModel Model { get; set; }
+
+        public string[] Roles { get; } = { "UserSocialMediaAddress.Update" };
+
+        public class UpdateUserSocialMediaAddressCommandHandler : IRequestHandler<UpdateUserSocialMediaAddressCommand,
+                                                                                UpdatedUserSocialMediaAddressDto>
+        {
+            private readonly IUserSocialMediaAddressRepository _userSocialMediaAddressRepository;
+            private readonly IMapper _mapper;
+            private readonly UserSocialMediaAddressBusinessRules _socialMediaBusinessRules;
+
+            public UpdateUserSocialMediaAddressCommandHandler(IUserSocialMediaAddressRepository userSocialMediaAddressRepository,
+                                                               IMapper mapper,
+                                                               UserSocialMediaAddressBusinessRules socialMediaBusinessRules)
+            {
+                _userSocialMediaAddressRepository = userSocialMediaAddressRepository;
+                _mapper = mapper;
+                _socialMediaBusinessRules = socialMediaBusinessRules;
+            }
+
+            public async Task<UpdatedUserSocialMediaAddressDto> Handle(UpdateUserSocialMediaAddressCommand request,
+                                                                 CancellationToken cancellationToken)
+            {
+                UserSocialMediaAddress? userSocialMediaAddress = await _userSocialMediaAddressRepository.GetAsync(
+                                                               u => u.Id == request.Id);
+
+                await _socialMediaBusinessRules.SocialMediaAddressExists(userSocialMediaAddress);
+                await _socialMediaBusinessRules.UserIdCanNotBeDuplicatedWhenUpdated(request);
+                await _socialMediaBusinessRules.GithubUrlCanNotBeDuplicatedWhenUpdated(request);
+
+                userSocialMediaAddress = _mapper.Map<UserSocialMediaAddress>(request.Model);
+                userSocialMediaAddress = await _userSocialMediaAddressRepository.UpdateAsync(userSocialMediaAddress);
+                UpdatedUserSocialMediaAddressDto mappedSMAddressDto = _mapper.Map<UpdatedUserSocialMediaAddressDto>(userSocialMediaAddress);
+                return mappedSMAddressDto;
+            }
+        }
+    }
+}
---
+++ src/demoProjects/Kodlama.io.Devs/Application/Features/UserSocialMediaAddresses/Commands/Update/UpdateUserSocialMediaAddressValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.UserSocialMediaAddresses.Commands.Update
+{
+    public class UpdateUserSocialMediaAddressValidator : AbstractValidator<UpdateUserSocialMediaAddressCommand>
+    {
+        public UpdateUserSocialMediaAddressValidator()
+        {
+            RuleFor(c => c.Id).NotEmpty();
+            RuleFor(c => c.Id).GreaterThan(0);
+
+            RuleFor(c => c.Model.UserId).NotEmpty();
+            RuleFor(c => c.Model.UserId).GreaterThan(0);
+
+            RuleFor(c => c.Model.GithubUrl).NotEmpty();
+
+        }
+    }
+}
