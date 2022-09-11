52983ef953bed96ad40b8d11376f933d704c2c5b
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2022-09-11 17:01:12 +0400

    day 4 all task complete

--- src/demoProjects/Kodlama.io.Devs/WebAPI/Controllers/UserSocialMediaAddressesController.cs
+++ src/demoProjects/Kodlama.io.Devs/WebAPI/Controllers/UserSocialMediaAddressesController.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.UserSocialMediaAddresses.Commands.Create;
+using Application.Features.UserSocialMediaAddresses.Commands.Delete;
+using Application.Features.UserSocialMediaAddresses.Commands.Update;
 using Application.Features.UserSocialMediaAddresses.Dtos;
 using Application.Features.UserSocialMediaAddresses.Models.Commands;
 using Microsoft.AspNetCore.Http;
@@ -17,5 +19,23 @@ namespace WebAPI.Controllers
             CreatedUserSocialMediaAddressDto result = await Mediator.Send(command);
             return Created("",result);
         }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromQuery] int id,
+                                                [FromBody] UpdateUserSocialMediaAddressModel model)
+        {
+            UpdateUserSocialMediaAddressCommand command = new() {Id=id, Model = model };
+            UpdatedUserSocialMediaAddressDto result = await Mediator.Send(command);
+            return Created("", result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery]int id)
+        {
+            DeleteUserSocialMediaAddressCommand command = new() { Id = id };
+            DeletedUserSocialMediaAddressDto result = await Mediator.Send(command);
+            return Created("", result);
+        }
+
     }
 }
